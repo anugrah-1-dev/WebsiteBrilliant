9b22567893536eb10f6427bf3b71bef958434565
anugrah-1-dev <tejomaliki1@gmail.com>
2026-05-02 16:29:53 +0000

    bismillah

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit7b0766fca7c36c737df120c0276be11c
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit7b0766fca7c36c737df120c0276be11c', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit7b0766fca7c36c737df120c0276be11c', 'loadClassLoader'));
